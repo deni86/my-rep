9c7300abeea62a5f44f42d2f9b6152a4aae24d41
Deni Rustemovic <rustemovic86@hotmail.com>
2016-11-01 22:10:07 +0100

    Initial project commit

---
+++ src/main/java/com/myrep/MyrepApplication.java
@@ -0,0 +1,12 @@
+package com.myrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrepApplication.class, args);
+	}
+}
